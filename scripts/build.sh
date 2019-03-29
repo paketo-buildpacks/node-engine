42679372a957c2f93c74d33db91f4eaae21fd82a
Daniel Thornton <dthornton@pivotal.io>
2019-03-29 15:27:15 -0400

    Change go build output dir to conform to pack spec
    Co-authored-by: Don Nguyen <dnguyen@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,10 @@
 set -exuo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
-
 TARGET_OS=${1:-linux}
 
 for b in cmd/*; do
     echo -n "Building $b..."
-    GOOS=$TARGET_OS go build -mod=vendor -ldflags="-s -w" -o bin/$b $b/main.go
+    GOOS=$TARGET_OS go build -mod=vendor -ldflags="-s -w" -o bin/$(basename $b) $b/main.go
     echo "done"
 done
