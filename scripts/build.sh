8cf7c2cbe25d579dab00cb6b8cbb583708c17ed8
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2019-04-01 14:49:43 +0000

    Update CNB tools

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,11 @@
 set -exuo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
 TARGET_OS=${1:-linux}
 
-for b in cmd/*; do
+for b in $(ls cmd); do
     echo -n "Building $b..."
-    GOOS=$TARGET_OS go build -mod=vendor -ldflags="-s -w" -o bin/$(basename $b) $b/main.go
+    GOOS=$TARGET_OS go build -mod=vendor -ldflags="-s -w" -o bin/$b cmd/$b/main.go
     echo "done"
 done
