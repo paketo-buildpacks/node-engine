efba3cee01c7493286051df28a0eb0a04e721136
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2019-04-01 18:58:21 +0000

    Update CNB tools

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -20,6 +20,18 @@ if [ "$#" -eq 1  ]; then
     PACK_VERSION="$1"
 fi
 
+
+install_pack_master() {
+    if [[ -f ".bin/pack" ]]; then return 0; fi
+
+    git clone https://github.com/buildpack/pack ./.bin/pack-repo
+    pushd .bin/pack-repo
+        go build -o ../pack ./cmd/pack/main.go
+    popd
+
+    rm -rf .bin/pack-repo
+}
+
 install_pack() {
     OS=$(uname -s)
 
@@ -75,18 +87,12 @@ expand() {
     rm $PACK_ARTIFACT
 }
 
-configure_pack() {
-    pack add-stack org.cloudfoundry.stacks.cflinuxfs3 \
-        --build-image cfbuildpacks/cflinuxfs3-cnb-experimental:build \
-        --run-image cfbuildpacks/cflinuxfs3-cnb-experimental:run || echo "Ignoring add stack error"
-}
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 mkdir -p .bin
 export PATH=$(pwd)/.bin:$PATH
 
-install_pack
-configure_pack
+install_pack_master
 install_packager
 
