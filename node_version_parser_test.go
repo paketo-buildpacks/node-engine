717284e5f20640a881ab8fbd7a8b5b2adc7670e6
Adam Barcan <adam@greenhouse.io>
2021-02-17 10:25:39 -0500

    Write failing node_version_parser_spec:
    * Got test cases for accetable .node-version formats from
    https://github.com/shadowspawn/node-version-usage
    * Cases are a lot more narrow than for `.nvmrc` bc `.node-version`
    is used with a variety of version managers that may have different
    logic for aliasing`

---
+++ node_version_parser_test.go
@@ -0,0 +1,90 @@
+package nodeengine_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	nodeengine "github.com/paketo-buildpacks/node-engine"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+)
+
+func testNodeVersionParser(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect = NewWithT(t).Expect
+
+		path   string
+		parser nodeengine.NodeVersionParser
+	)
+
+	it.Before(func() {
+		file, err := ioutil.TempFile("", ".node-version")
+		Expect(err).NotTo(HaveOccurred())
+		defer file.Close()
+
+		path = file.Name()
+
+		parser = nodeengine.NewNodeVersionParser()
+	})
+
+	it.After(func() {
+		Expect(os.RemoveAll(path)).To(Succeed())
+	})
+
+	it("returns a version constraint", func() {
+		testCases := map[string]string{
+			"10.2":    "10.2.*",
+			"10.2.3":  "10.2.3",
+			"v10.2.3": "10.2.3",
+		}
+
+		for input, output := range testCases {
+			err := ioutil.WriteFile(path, []byte(input), 0644)
+			Expect(err).NotTo(HaveOccurred())
+
+			version, err := parser.ParseVersion(path)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(version).To(Equal(output), fmt.Sprintf("input of %q failed to produce output of %q", input, output))
+		}
+	})
+
+	context("when the .node-version file does not exist", func() {
+		it.Before(func() {
+			Expect(os.RemoveAll(path)).To(Succeed())
+		})
+
+		it("returns an empty version", func() {
+			version, err := parser.ParseVersion(path)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(version).To(BeEmpty())
+		})
+	})
+
+	context("failure cases", func() {
+		context("when the .node-version is empty", func() {
+			it.Before(func() {
+				Expect(os.Truncate(path, 0)).To(Succeed())
+			})
+
+			it("returns an error", func() {
+				_, err := parser.ParseVersion(path)
+				Expect(err).To(MatchError("invalid version specified in .node-version: \"\""))
+			})
+		})
+
+		context("when the .node-version contains a malformed semver number", func() {
+			it.Before(func() {
+				err := ioutil.WriteFile(path, []byte("1.2.this is not a number"), 0644)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			it("returns an error", func() {
+				_, err := parser.ParseVersion(path)
+				Expect(err).To(MatchError("invalid version specified in .node-version: \"1.2.this is not a number\""))
+			})
+		})
+	})
+}
