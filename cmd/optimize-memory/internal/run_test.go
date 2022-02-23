ef030867f4179b64180b456b098df2a9159a1fc5
Ryan Moran <rmoran@vmware.com>
2022-02-23 10:21:19 -0800

    Removes pre-v2 packit imports

--- cmd/optimize-memory/internal/run_test.go
+++ cmd/optimize-memory/internal/run_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/sclevine/spec"
 
 	. "github.com/onsi/gomega"
-	. "github.com/paketo-buildpacks/packit/matchers"
+	. "github.com/paketo-buildpacks/packit/v2/matchers"
 )
 
 func testRun(t *testing.T, context spec.G, it spec.S) {
